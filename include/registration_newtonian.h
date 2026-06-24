829975baf9d88a05479d72cf97d878d882ba5a6a
Jamie Thomas McCabe <jamietjjmccabe@gmail.com>
2026-06-24 08:04:29 +0100

    Add regular Newtonian perturbation API

---
+++ include/registration_newtonian.h
@@ -0,0 +1,58 @@
+#ifndef REGISTRATION_NEWTONIAN_H
+#define REGISTRATION_NEWTONIAN_H
+
+#include "registration_transfer.h"
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+  double scale_factor;
+  double conformal_hubble;
+  double k_squared;
+  double temperature_gev;
+
+  double rho_flux;
+  double rho_registration;
+
+  double delta_flux;
+  double delta_rho_registration;
+  double q_registration;
+
+  double phi;
+  double psi_prime;
+  double theta_transfer;
+  double delta_rho_thermal_over_rho_thermal;
+} registration_newtonian_input;
+
+typedef struct {
+  registration_transfer_state transfer;
+
+  double delta_theta_over_theta;
+  double delta_temperature_over_temperature;
+  double delta_Q;
+
+  double delta_flux_prime;
+  double delta_rho_registration_prime;
+  double q_registration_prime;
+
+  double energy_bianchi_residual;
+  double momentum_balance_residual;
+} registration_newtonian_output;
+
+int registration_newtonian_derivs(
+    const registration_eos_table *table,
+    const registration_transfer_parameters *parameters,
+    const registration_newtonian_input *input,
+    registration_newtonian_output *output,
+    char *error_message,
+    size_t error_message_size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
